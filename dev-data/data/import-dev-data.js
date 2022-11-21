4752454acbeab5f6772b9981b7d8b7b5e94b66cf
David Martin <dmartin5150@icloud.com>
2022-11-20 22:04:55 -0600

    uploaded tour data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//server
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection succesful!'));
+
+//Read data
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// import data into database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data succesfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit(0);
+};
+
+// Delete all data from a collection
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data succesfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit(0);
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}

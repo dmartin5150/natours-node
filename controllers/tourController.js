a7652486e11f0d4bb6fd6c2d79e6b8e9f577e456
David Martin <dmartin5150@icloud.com>
2022-11-24 16:10:15 -0600

    refactored API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 // const { query } = require('express')
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apifeatures');
 
 // exports.checkBody = (req, res, next) => {
 //   if (!req.body.name || !req.body.price) {
@@ -20,44 +21,12 @@ exports.aliasTopTours = async (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // {difficulty: 'easy', duration: {$gte:5}}
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) {
-        throw new Error('This page does not exist');
-      }
-    }
-
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
 
     res.status(200).json({
